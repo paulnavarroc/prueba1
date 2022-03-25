68a315ed53f9355b68c2c12d9286ad2c3f15839e
paulnavarroc <102325540+paulnavarroc@users.noreply.github.com>
2022-03-25 16:18:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -23,14 +23,42 @@ $db_name = 'pruebadb';
            <td>name</td>
            <td>quantity</td>
        </tr>
-       <?php if(isset($producto)){
-        foreach ($producto as $key) {?>
-           <tr>
-           <td><?php echo $key['id']; ?></td>
-           <td><?php echo $key['name']; ?></td>
-           <td><?php echo $key['quantity']; ?></td>
-           </tr>
-       <?php } print_r($producto); } ?>
+       <?php 
+        
+        
+            $conn = mysqli_init();
+
+            //mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+            // Establish the connection
+            mysqli_real_connect($conn, 'myservidor.mysql.database.azure.com', 'pauln@myservidor', 'Paul.123', 'pruebadb', 3306, NULL, MYSQLI_CLIENT_SSL);
+
+            //If connection failed, show the error
+            if (mysqli_connect_errno())
+            {
+                die('Failed to connect to MySQL: '.mysqli_connect_error());
+            }
+
+
+            printf("Reading data from table: \n");
+            $res = mysqli_query($conn, 'SELECT * FROM inventory');
+            while ($row = mysqli_fetch_assoc($res))
+             {
+                var_dump($row);
+             }
+            $producto[]=mysqli_fetch_assoc($res);
+        
+        
+        
+        
+                if(isset($producto)){
+                foreach ($producto as $key) {?>
+                   <tr>
+                   <td><?php echo $key['id']; ?></td>
+                   <td><?php echo $key['name']; ?></td>
+                   <td><?php echo $key['quantity']; ?></td>
+                   </tr>
+               <?php } print_r($producto); } ?>
     </table>
 </body>
 </html>
