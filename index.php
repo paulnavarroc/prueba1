e86314628f4646345a2a033542f2eea66dd89588
paulnavarroc <102325540+paulnavarroc@users.noreply.github.com>
2022-03-25 15:16:16 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $host = 'myservidor.mysql.database.azure.com';
 $username = 'pauln@myservidor';
 $password = 'Paul.123';
@@ -25,4 +26,32 @@ while ($row = mysqli_fetch_assoc($res))
  {
     var_dump($row);
  }
+$_SESSION['producto'][]=$row;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grupo 5</title>
+</head>
+<body>
+    <table border="1">
+       <tr>
+           <td>id</td>
+           <td>name</td>
+           <td>quantity</td>
+       </tr>
+       <?php if(isset($_SESSION['producto'])){
+        foreach ($_SESSION['producto'] as $key => $value) {?>
+           <tr>
+           <td><?php echo $value['id']; ?></td>
+           <td><?php echo $value['name']; ?></td>
+           <td><?php echo $value['quantity']; ?></td>
+           </tr>
+       <?php } } ?>
+    </table>
+</body>
+</html>
