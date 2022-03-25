e03318eb0d9e9c4e309075a7270be479bed6f9b4
paulnavarroc <102325540+paulnavarroc@users.noreply.github.com>
2022-03-25 16:33:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,22 @@ $password = 'Paul.123';
 $db_name = 'pruebadb';
 
 //Initializes MySQLi
+$conn = mysqli_init();
 
+            //mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+            // Establish the connection
+            mysqli_real_connect($conn, 'myservidor.mysql.database.azure.com', 'pauln@myservidor', 'Paul.123', 'pruebadb', 3306, NULL, MYSQLI_CLIENT_SSL);
+
+            //If connection failed, show the error
+            if (mysqli_connect_errno())
+            {
+                die('Failed to connect to MySQL: '.mysqli_connect_error());
+            }
+
+
+            printf("Reading data from table: \n");
+            $res = mysqli_query($conn, 'SELECT * FROM inventory');
 ?>
 
 <!DOCTYPE html>
@@ -26,22 +41,7 @@ $db_name = 'pruebadb';
        <?php 
         
         
-            $conn = mysqli_init();
-
-            //mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
-
-            // Establish the connection
-            mysqli_real_connect($conn, 'myservidor.mysql.database.azure.com', 'pauln@myservidor', 'Paul.123', 'pruebadb', 3306, NULL, MYSQLI_CLIENT_SSL);
-
-            //If connection failed, show the error
-            if (mysqli_connect_errno())
-            {
-                die('Failed to connect to MySQL: '.mysqli_connect_error());
-            }
-
-
-            printf("Reading data from table: \n");
-            $res = mysqli_query($conn, 'SELECT * FROM inventory');
+            
             while ($row = mysqli_fetch_assoc($res))
              {?>
                 <tr>
